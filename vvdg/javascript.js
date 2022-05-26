100588fea324cfa9f40c43cd79464829cffed773
Lenix31 <69859234+Lenix31@users.noreply.github.com>
2022-05-26 20:30:01 +0300

    Add files via upload

---
+++ vvdg/javascript.js
@@ -0,0 +1,16 @@
+function test()
+{
+    let word = document.querySelector("#variable").value;
+    alert(word)
+}
+
+function countdown()
+{
+    var timer = 60;
+
+var interval = setInterval(function() {
+    timer--;
+    $('timer').text(timer);
+    if (timer === 0) clearInterval(interval);
+}, 1000);
+}
