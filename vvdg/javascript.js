896dbc128b89f7045116dc4857e616f13e602b2a
Lenix31 <69859234+Lenix31@users.noreply.github.com>
2022-05-30 20:46:54 +0300

    Add files via upload

--- vvdg/javascript.js
+++ vvdg/javascript.js
@@ -1,16 +1,141 @@
-function test()
-{
-    let word = document.querySelector("#input1").value;
-    alert(word)
+document.addEventListener("DOMContentLoaded", function()
+{    
+    document.querySelector('#start').addEventListener('click', () => countdown());
+});
+
+
+
+// function test()
+// {
+//     let word = document.querySelector("#input1").value;
+//     alert(word)
+// }
+
+
+
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    var refreshIntervalId;
+    refreshIntervalId = setInterval(function () {
+  
+        seconds = parseInt(timer % 60, 10);
+
+        
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent =  seconds;
+
+        if (--timer < 0) {
+            
+            
+            main();
+            clearInterval(refreshIntervalId);
+        }
+        
+    }, 1000);
+    
 }
 
-function countdown()
-{
-    var timer = 60;
+function countdown() {
+    var seconds = 5,
+        display = document.querySelector('#time');
+    
+    startTimer(seconds, display);
+    return;
+};
 
-var interval = setInterval(function() {
-    timer--;
-    $('timer').text(timer);
-    if (timer === 0) clearInterval(interval);
-}, 1000);
-}
+
+
+
+
+// readability
+
+
+
+
+
+    // get input from user
+    function main()
+    {
+        let text = document.querySelector("#input1").value;
+        
+        let numLetters = getNumLetters(text);
+        let numWords = getNumWords(text);
+        let numSentences = getnumSentences(text);
+    
+        let gradeLevel = getGradeLevel(numLetters, numSentences, numWords);
+    
+        printGradeLevel(gradeLevel);
+    }
+    
+    
+
+
+
+
+    function getNumLetters(text)
+    {
+        // loop through text, counting number of letters, words and sentences
+        var l = 0
+        for (let i = 0; i<`${text.length}`; i ++)
+        {
+            if (text[i].charCodeAt(0) > 64 && text[i].charCodeAt(0) < 123) // check is letter
+            {
+                l = l + 1;
+            }
+            
+        }
+        return l;
+    }  
+    function getNumWords(text)
+    {
+        var w = 0;
+        for (let i = 0; i<`${text.length}`; i ++)
+        {
+            if (text[i].charCodeAt(0) == 32 ) 
+            {
+                w = w + 1;
+            }
+            
+        }
+        w = w + 1;
+        return w;
+    }
+    function getnumSentences(text)
+    {
+        var s= 0;
+        for (let i = 0; i<`${text.length}`; i ++)
+        {
+            if (text[i].charCodeAt(0) == 46 || text[i].charCodeAt(0) == 33 || text[i].charCodeAt(0) == 63) 
+            {
+                s = s + 1;
+            }
+            
+        }
+        return s;
+    }
+    function getGradeLevel(numLetters, numSentences, numWords)
+    {
+        var L = numLetters / numWords * 100
+        var S = numSentences / numWords * 100
+        var index = Math.round(0.0588 * L - 0.296 * S - 15.8)
+        return index;
+    }
+    function printGradeLevel(gradeLevel)
+    {
+        display = document.querySelector('#results');
+
+        var x = 1;
+        if(gradeLevel>= 4)
+        {
+            display.textContent =  "You are smarter than a 10 year old! Congratulations!";
+        }
+        else
+        {
+            display.textContent =  "You're almost there, try again!";
+        }
+        
+    }
+
+     
+    
--- vvdg/readability.js
+++ vvdg/readability.js
@@ -4,43 +4,90 @@ document.addEventListener("DOMContentLoaded", function()
 });
 
     // get input from user
-    const len = document.getElementById("input1").value;
+    // const len = document.getElementById("input1").value;
     function main()
     {
         let text = document.querySelector("#input1").value;
         
         let numLetters = getNumLetters(text);
-        // let numWords = getNumWords(text);
-        // let numSentences = getnumSentences(text);
+        let numWords = getNumWords(text);
+        let numSentences = getnumSentences(text);
     
-        // let gradeLevel = getGradeLevel(numLetters, numSentences, numWords);
+        let gradeLevel = getGradeLevel(numLetters, numSentences, numWords);
     
-        // printGradeLevel(gradeLevel);
+        printGradeLevel(gradeLevel);
     }
     
     
+
+
+
+
     function getNumLetters(text)
     {
         // loop through text, counting number of letters, words and sentences
         var l = 0
-        for (let i = 0; i<len.lenght; i ++)
+        for (let i = 0; i<`${text.length}`; i ++)
         {
-            if ((text > 64 && text < 91) || (text > 96 && text < 123)) // check is letter
+            if (text[i].charCodeAt(0) > 64 && text[i].charCodeAt(0) < 123) // check is letter
             {
                 l = l + 1;
             }
             
         }
-        console.log(l);
         return l;
     }  
+    function getNumWords(text)
+    {
+        var w = 0;
+        for (let i = 0; i<`${text.length}`; i ++)
+        {
+            if (text[i].charCodeAt(0) == 32 ) 
+            {
+                w = w + 1;
+            }
+            
+        }
+        w = w + 1;
+        return w;
+    }
+    function getnumSentences(text)
+    {
+        var s= 0;
+        for (let i = 0; i<`${text.length}`; i ++)
+        {
+            if (text[i].charCodeAt(0) == 46 || text[i].charCodeAt(0) == 33 || text[i].charCodeAt(0) == 63) 
+            {
+                s = s + 1;
+            }
+            
+        }
+        return s;
+    }
+    function getGradeLevel(numLetters, numSentences, numWords)
+    {
+        var L = numLetters / numWords * 100
+        var S = numSentences / numWords * 100
+        var index = Math.round(0.0588 * L - 0.296 * S - 15.8)
+        return index;
+    }
+    function printGradeLevel(gradeLevel)
+    {
+        if(gradeLevel<1)
+        {
+            console.log("Before Grade 1")
+            return;
+        }
+        else if (gradeLevel >= 16)
+        {
+            console.log("Grade 16+")
+            return;
+        }
+        console.log("Grade " + gradeLevel);
+    }
     
     
     
     
     
-
-
-
-
 
