9da61438fecf877aafbd4509d45cb05568173788
Lenix31 <69859234+Lenix31@users.noreply.github.com>
2022-06-04 17:52:13 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -37,7 +37,7 @@ function startTimer(duration, display) {
 }
 
 function countdown() {
-    var seconds = 10;
+    var seconds = 60;
         display = document.querySelector('#time');
     
     startTimer(seconds, display);
